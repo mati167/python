0278f119c8df8c52d948af7831a563be959477c7
mati167 <gonzalezromeromatias@gmail.com>
2019-12-06 14:52:39 -0300

    prediccion

--- deep_learning/red_neuronal_artificial.py
+++ deep_learning/red_neuronal_artificial.py
@@ -40,10 +40,37 @@ clasificador = Sequential()
 #agregamos primera capa input
 clasificador.add(Dense(units=6,kernel_initializer='uniform',activation='relu',input_dim=11))
 clasificador.add(Dense(units=6,kernel_initializer='uniform',activation='relu'))
+
 #
-y_predic = classifier.predict(x_test)
+clasificador.add(Dense(units=1,kernel_initializer='uniform',activation='sigmoid'))
+#
+clasificador.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+#
+clasificador.fit(x_train,y_train,batch_size=10,epochs=100)
+
+#
+y_pred = clasificador.predict(x_test)
+y_pred = (y_pred>0.5)
 
 #
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test,y_pred)
+
+# Prediciendo Nuevo Cliente
+"""
+Geografía: Francia
+Puntaje de crédito: 600
+Género: Masculino
+Edad: 40 años
+Tenencia: 3 años
+Saldo: $60000
+Número de productos: 2
+¿Tiene este cliente una tarjeta de crédito? Sí
+¿Este cliente es un miembro activo? Si
+Salario estimado: $50000
+"""
+
+nuevo_cliente = clasificador.predict(sc_x.transform(np.array([[0,0,600,1,40,3,60000,2,1,1,50000]])))
+nuevo_cliente = (nuevo_cliente>0.5)
+
 
